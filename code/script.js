6e1295634a8664acd9db115d210e597cb8ac908f
Charlotte Johansson <charlotte@verismohr.com>
2022-08-22 17:54:44 +0200

    Added response to Joke or Riddle

--- code/script.js
+++ code/script.js
@@ -1,14 +1,27 @@
 // Variables that point to selected DOM elements
-const chat = document.getElementById('chat');
+const chat = document.getElementById('chat')
+const inputWrapper = document.getElementById('input-wrapper')
+const input = document.getElementById('input')
+const sendBtn = document.getElementById('send')
+
+let questionNumber = 1
 
 // If you need any global variables that you can use across different functions, declare them here:
 
+
+const botReply = (msg) => {
+  showMessage(msg, 'bot')
+}
+
+const userReply = (msg) => {
+  showMessage(msg, 'user')
+}
 
 // Declare your functions after this comment
 
 // This function will add a chat bubble in the correct place based on who the sender is
 const showMessage = (message, sender) => {
-  // the if statement checks if the sender is 'user' and if that's the case it inserts an html senction inside the chat with the posted message
+  // the if statement checks if the sender is 'user' and if that's the case it inserts an html section inside the chat with the posted message
   if (sender === 'user') {
     chat.innerHTML += `
       <section class="user-msg">
@@ -18,7 +31,7 @@ const showMessage = (message, sender) => {
         <img src="assets/user.png" alt="User" />  
       </section>
     `
-    // the else if statement checks if the sender is a bot and if that's the case it inserts an html senction inside the chat with the posted message
+    // the else if statement checks if the sender is a bot and if that's the case it inserts an html section inside the chat with the posted message
   } else if (sender === 'bot') {
     chat.innerHTML += `
       <section class="bot-msg">
@@ -29,16 +42,46 @@ const showMessage = (message, sender) => {
       </section>
     `
   }
+
   // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
   chat.scrollTop = chat.scrollHeight;
+}
+
+const nextQuestion = (message) => {
+  console.log('questionNumber', questionNumber)
+
+  if (questionNumber === 1) {
+    userReply(message)
+    input.value = ''
+    setTimeout(() => showJokeorRiddle(message), 1000)}
+  else if (questionNumber === 2) {
+    userReply(message)
+    setTimeout(() => showJoke(message), 1000)
+} else if (questionNumber === 3) {
+  userReply(message)
+  setTimeout(() => showRiddle(message), 1000)
+  }
 }
 
 // Starts here
 const greeting = () => {
   // here we call the function showMessage, that we declared earlier with the argument "Hello there, What's your name?" for message, and the argument "bot" for sender
-  showMessage("Hello there, What's your name?", 'bot');
-  // Just to check it out, change 'bot' to 'user' here 👆
+  showMessage("Hi! Would you like a joke or a riddle?", 'bot');
+
+  document
+    .getElementById('jokeBtn')
+    .addEventListener('click', () => nextQuestion('Joke'))
+  document
+    .getElementById('riddleBtn')
+    .addEventListener('click', () => nextQuestion('Riddle'))
 }
+
+const showJoke = (type) => {
+  questionNumber++
+
+  botReply (`Oh so you are in the mood for ${type}?`)
+}
+
 
 // Set up your eventlisteners here
 
@@ -48,4 +91,4 @@ const greeting = () => {
 // But if we want to add a little delay to it, we can wrap it in a setTimeout:
 // setTimeout(functionName, timeToWaitInMilliSeconds)
 // This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000);
+setTimeout(greeting);
